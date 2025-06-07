acfab947d74e1bf7bd537c51600354b3ddb83e88
ydkdan6 <ydkdan6@gmail.com>
2025-06-07 20:28:00 +0100

    fixed n/a

--- src/components/admin/PayoutManagement.tsx
+++ src/components/admin/PayoutManagement.tsx
@@ -593,9 +593,9 @@ const PayoutManagement = () => {
                         </div>
                       </td>
                       <td className="px-6 py-4 whitespace-nowrap">
-                        <div className="text-sm text-gray-900">
-                          {earning.drivers?.plate_number || 'N/A'}
-                        </div>
+                        {/* <div className="text-sm text-gray-900">
+                          {earning.plate_number || 'N/A'}
+                        </div> */}
                         <div className="text-xs text-blue-600 font-medium mt-1">
                           {earning.drivers?.payout_frequency || 'weekly'} payouts
                         </div>
